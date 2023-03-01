fd28d01619053a7b19fcb18b03df3fe05882faa5
Liopin <contact.pinheiro.lionel@gmail.com>
2023-03-01 15:21:48 +0100

    Setup an eleventy project

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,10 @@
-const { EleventyI18nPlugin } = require("@11ty/eleventy");
-
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(EleventyI18nPlugin, {
-    // any valid BCP 47-compatible language tag is supported
-    defaultLanguage: "", // Required, this site uses "en"
-  });
+  return {
+    dir: {
+      input: 'src',
+      output: 'dist'
+    },
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk'
+  };
 };
