177519c6f5ad2a789ced5e4341e2f63d714c0099
Liopin <contact.pinheiro.lionel@gmail.com>
2023-03-01 16:03:10 +0100

    Modify .eleventy to manage layouts
      - Create _date/ and _includes/
      To manage futur data and layouts

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,12 @@ module.exports = function(eleventyConfig) {
   return {
     dir: {
       input: 'src',
-      output: 'dist'
+      output: 'dist',
+      includes: "_includes",
     },
+    templateFormats:        ['md', 'njk'],
     markdownTemplateEngine: 'njk',
-    htmlTemplateEngine: 'njk'
+    htmlTemplateEngine:     'njk',
+    passtroughFileCopy:     true,
   };
 };
